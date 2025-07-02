e3d91d997e0bdc3fe55f474fed1f3d4a72767a8e
Sugumar <34702403+sugumar101@users.noreply.github.com>
2025-07-02 14:20:32 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+console.log("file updated");
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 4000);
